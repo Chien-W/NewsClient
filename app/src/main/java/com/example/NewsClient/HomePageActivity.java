d092cc3ca023438c3099a36f2626c67c2151ffb3
Chien-W <48709148+Chien-W@users.noreply.github.com>
2020-11-21 12:17:16 +0800

    V1.0

--- app/src/main/java/com/example/NewsClient/HomePageActivity.java
+++ app/src/main/java/com/example/NewsClient/HomePageActivity.java
@@ -1,21 +1,39 @@
 package com.example.NewsClient;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.NewsClient.gson.Data;
 import com.example.NewsClient.gson.News;
@@ -104,6 +122,7 @@ public class HomePageActivity extends Activity implements AdapterView.OnItemClic
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                requestPermissino();
                 Log.d("点击成功：", "点击成功");
             }
         });
@@ -134,7 +153,7 @@ public class HomePageActivity extends Activity implements AdapterView.OnItemClic
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("http://v.juhe.cn/toutiao/index?type="+part+"&key=a022eb1dafc57af1e06a19a29a61456a")
+                            .url("https://www.oyyandwjw.xyz/xw.json")
                             .build();
                     Response response = null;
                     response = client.newCall(request).execute();
@@ -209,5 +228,119 @@ public class HomePageActivity extends Activity implements AdapterView.OnItemClic
             return view;
         }
     }
+    public static final int CHOOSE_PHOTO = 2;
+    String imagePath = null;
 
+
+    private void requestPermissino() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }else {
+            openAlbum();
+        }
+
+    }
+
+    private void openAlbum(){
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO); //打开相册
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this,"你拒绝了该权限",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case CHOOSE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    //判断手机系统版本号
+                    if(Build.VERSION.SDK_INT>=19){
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitKat(Intent data){
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];  //解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID+"="+id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath = getImagePath(uri,null);
+        }else if("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath); //根据图片路径显示图片
+    }
+
+    //将选择的图片Uri转换为路径
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor!= null){
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    //展示图片
+    private void displayImage(String imagePath){
+        if(imagePath!=null && !imagePath.equals("")){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            imageView.setImageBitmap(bitmap);
+            //存储上次选择的图片路径，用以再次打开app设置图片
+            SharedPreferences sp = getSharedPreferences("sp_img",MODE_PRIVATE);  //创建xml文件存储数据，name:创建的xml文件名
+            SharedPreferences.Editor editor = sp.edit(); //获取edit()
+            editor.putString("imgPath",imagePath);
+            editor.apply();
+        }else {
+            Toast.makeText(this,"获取图片失败",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //设置再次app时显示的图片
+        SharedPreferences sp = getSharedPreferences("sp_img", MODE_PRIVATE);
+        //取出上次存储的图片路径设置此次的图片展示
+        String beforeImagePath = sp.getString("imgPath", null);
+        displayImage(beforeImagePath);
+    }
 }
